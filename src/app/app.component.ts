4c92b5a7f0adb932d237813fb64651555e9691cc
Purushotthama B V <purushothamacharya@gmail.com>
2023-10-03 20:07:38 +0530

    rete js installation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Injector, ViewChild } from '@angular/core'
+
+import { createEditor } from './rete'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'rete';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('rete') container!: ElementRef<HTMLElement>
+
+  constructor(private injector: Injector) { }
+
+  async ngAfterViewInit() {
+    await createEditor(this.container.nativeElement, this.injector)
+  }
 }
